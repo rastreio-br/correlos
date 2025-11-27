60104d258ce49f59c3b14eb43934e4010fd9ad09
mercado <awsjaneiro25@gmail.com>
2025-11-27 16:18:51 -0300

    Add files via upload

---
+++ 6ht6jj4299/j837101.js
@@ -0,0 +1,2 @@
+var key = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789:/.-!@#$%^&*()_+';
+var shiftedKey = '5si)A/y+89gQV@YSFoN4M0^KjEI2d_wHXrbaBe!O*3Tp%CRW:tDP#(lcGhuxUkfqZ&$J7z.L1mn6v-';
